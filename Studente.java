00d15967e5ef4abd61e5cdcc50079865a4b9361b
SaraCabi <cabiddu.sara@iisbuonarrotivolta.it>
2025-12-10 20:34:11 +0100

    Add necessary imports for Studente class
    Added imports for file handling and Scanner usage.

--- Studente.java
+++ Studente.java
@@ -1,1 +1,12 @@
+import java.io.IoException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
+public class Studente{
+  private final String nome;
+  private final String cognome;
+  private final String matricola;
+  private final int annoNascita;
